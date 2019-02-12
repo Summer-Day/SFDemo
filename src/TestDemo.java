78571390ea0a682fae774108045e54a9b7683e1b
王硕 <wangshuo@shehuiwangdeMacBook-Pro.local>
2019-02-12 09:12:22 +0800

    初始化

---
+++ src/TestDemo.java
@@ -0,0 +1,9 @@
+/**
+ * Created by wangshuo on 2019/2/12.
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.print("1111");
+    }
+}
